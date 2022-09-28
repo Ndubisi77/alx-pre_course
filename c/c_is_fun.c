7e89d22cce249dd379c2989385251e870fff0b3b
Nduubisi Chidubem <ndubisichidubem@gmail.com>
2022-09-28 15:39:18 -0700

    my first commit

---
+++ c/c_is_fun.c
@@ -0,0 +1,8 @@
+
+root@efb23c0a9847:/alx-pre_course/c# ls
+root@efb23c0a9847:/alx-pre_course/c# rm cat>c_is_fun.c
+root@efb23c0a9847:/alx-pre_course/c# ls
+root@efb23c0a9847:/alx-pre_course/c# echo cat>c_is_fun.c
+root@efb23c0a9847:/alx-pre_course/c# ls
+root@efb23c0a9847:/alx-pre_course/c# echo c_is_fun.c>c.md
+root@efb23c0a9847:/alx-pre_course/c# ls
